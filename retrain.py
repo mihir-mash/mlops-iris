30e969eb1e0cf09b6bafb9cefffc7e930894674b
mihir-mash <mihirmashruwala@gmail.com>
2025-11-17 09:55:13 +0000

    Fix PyCaret setup: removed silent=True

--- retrain.py
+++ retrain.py
@@ -2,7 +2,7 @@ from pycaret.classification import *
 from pycaret.datasets import get_data
 
 iris = get_data('iris')
-exp = setup(iris, target='species', session_id=42, silent=True, html=False)
+exp = setup(iris, target='species', session_id=42,verbose = False,html=False)
 model = compare_models()
 final = finalize_model(model)
 save_model(final, 'iris_model')
